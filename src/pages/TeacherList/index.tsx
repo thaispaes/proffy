d42f489649211bbf8cb15b6141410807d6314ab1
thaispaes <thaisreginapaesdossantos@gmail.com>
2020-08-03 21:40:18 -0300

    Form para pesquisa Mobile
    Com ERROR no design

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import './styles.css';
+
+import PageHeader from '../../components/PageHeader';
+
 
 function TeacherList(){
     return(
-        <h1> Teacher List </h1>
+        <div id="page-teacher-list" className="container">
+          <PageHeader title="Estes são os proffys disponíveis.">
+               <form id="search-teachers" className="container">
+                   <div className="input-block">
+                       <label htmlFor="subject"> Materia </label>
+                       <input type="text" id="subject"/>
+                   </div>
+
+                   <div className="input-block">
+                       <label htmlFor="week-day"> Dia da Semana </label>
+                       <input type="text" id="week-day"/>
+                   </div>
+
+                   <div className="input-block">
+                       <label htmlFor="time"> Horario </label>
+                       <input type="text" id="time"/>
+                   </div>
+                   </form>
+            </ PageHeader>
+        </div>
     )
 }
 
